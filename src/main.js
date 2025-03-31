4d27d6ff1c05302373b170d7cbe334d916dd2572
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-31 12:34:13 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -3,19 +3,18 @@
 // let msg = document.getElementById("msg");
 // console.log(msg);
 
-let message = document.getElementsByClassName("message");
-console.log(message);
+// let message = document.getElementsByClassName("message");
+// console.log(message);
 
-let tag = document.getElementsByTagName("p");
-console.log(tag);
+// let tag = document.getElementsByTagName("p");
+// console.log(tag);
 
-let querySelector = document.querySelector(".querySelector");
-console.log(querySelector);
+// let querySelector = document.querySelector(".querySelector");
+// console.log(querySelector);
 
-let querySelectorAll = document.querySelectorAll("p");
-console.log(querySelectorAll);
+// let querySelectorAll = document.querySelectorAll("p");
+// console.log(querySelectorAll);
 
 
-let cont = document.querySelector(".container");
-let msg = cont.getElementById("msg");
-console.log(msg) ;
+l
+
