73e3269d6db40fb99a3e79119a327629ad148835
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-31 12:20:40 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let msg = document.getElementById("msg");
 console.log(msg);
 
 let message = document.getElementsByClassName("message");
-
+console.log(message);
 
 let tag = document.getElementsByTagName("p");
 console.log(tag);
