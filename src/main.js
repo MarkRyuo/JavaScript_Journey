bb553910bdc1558d351b715d5d60531374a1823a
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-31 21:08:34 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 
 
-// let msg = document.getElementById("msg");
-// console.log(msg);
 
-// let message = document.getElementsByClassName("message");
-// console.log(message);
-
-// let tag = document.getElementsByTagName("p");
-// console.log(tag);
-
-// let querySelector = document.querySelector(".querySelector");
-// console.log(querySelector);
-
-// let querySelectorAll = document.querySelectorAll("p");
-// console.log(querySelectorAll);
-
-
-let cont = document.getElementById("container");
-let msg = cont.querySelectorAll(".message");
-console.log(msg);
+// * This is getting element by id 
 
