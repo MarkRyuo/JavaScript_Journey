9b65afc1e4380591c1cb8fdc689d6a86d4f23ef6
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-03 08:04:20 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
 let heading1 = document.getElementById('heading1');
 console.log(heading1);
 
-// * This is gettiing element by class name
+// * This is getting element by class name
 
 let heading2 = document.getElementsByClassName("heading2");
 console.log(heading2);
