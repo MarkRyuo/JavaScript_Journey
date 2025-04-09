0895acf37fe08adbae596ad15778ed27ba1bb60f
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-09 16:37:21 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ const mainText = document.getElementById("main-text") ;
 console.log(mainText);
 
 
-
 //* getElementsByClassName(".className")
 
 const items = document.getElementsByClassName("items") ;
