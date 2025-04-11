7e3cf0b90a21660f16f33d9997f99a4976c76222
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-11 10:05:49 +0800

    Subject: Update

---
+++ src/main.js
@@ -0,0 +1,18 @@
+//* Traverse DOM
+
+//* Parent Node Traversal
+
+let ul = document.querySelector("ul");
+console.log(ul.parentNode);
+console.log(ul.parentElement);
+
+
+//* Child Node Traversal
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild)
+// console.log(ul.lastChild);
+console.log(ul.children);
+
+ul.children[0].style.textTransform = "uppercase";
+ul.children[1].style.color = "red";
