728c060cf705843d5a5969255e2a6824932481c5
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-11 10:16:43 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ let main  = document.querySelector("main");
 
 main.style.padding = "10px 20px";
 
-let div = document.querySelector("div");
+let div = document.querySelectorAll("div");
 div.style.border = "1px solid black"
