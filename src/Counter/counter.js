17ae453ef0ffbca309599927b6c75712110aaa63
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-13 05:44:01 +0800

    Subject: Update

--- src/Counter/counter.js
+++ src/Counter/counter.js
@@ -21,17 +21,21 @@ let btnCounter = document.querySelector('.btn-counter');
 
 const question = () => {
   let q1 = prompt("What is your name?");
+  let count = 5 ;
 
-  const count = 5 ;
-
-  while (q1 === null || q1 === "") {
-    let alertStorage = alert("Please enter your name");
-    
+  while (q1 === null || q1 === "") {    
     if(q1 === null || q1 === "") {
       q1 = prompt("Please enter your name");
       count -= 1;
     }
+
+    if (count === 0) {
+      alert("You have exceeded the number of attempts");
+      break;
+    }
   }
+
+  localStorage.setItem('name', q1);
 
 }
 
