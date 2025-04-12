1ab185bd8c94ac23f56ed998d1d303875d1f129a
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-12 16:30:26 +0800

    Subject: Update

--- src/main.js
+++ src/main.js
@@ -55,3 +55,11 @@ function iloveyou() {
   
   if(cardButtonSecondary === )
 }
+
+
+
+//* if(condition) {
+//*   // code to be executed if condition is true
+//* } else {
+//*   // code to be executed if condition is false
+//* }
